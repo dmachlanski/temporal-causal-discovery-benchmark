95fec1a017994e0244db0125f1a95380282b1d1a
dmachlanski <damian.machlanski@gmail.com>
2025-10-25 12:09:27 +0100

    Added synthetic experiment.

---
+++ workflow/scripts/data/prepare_synth.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+data = pd.read_csv(os.path.join(snakemake.params['data_opt']['path'], snakemake.params['opt']['seed'], 'data.csv'), header=None)
+graph = pd.read_csv(os.path.join(snakemake.params['data_opt']['path'], snakemake.params['opt']['seed'], 'graph.csv'), header=None)
+
+cols = ['x', 'y']
+
+# drop the index, don't need it
+data = data.drop(columns=0)
+
+# name columns
+data.columns = cols
+graph.columns = cols
+
+# subsampling factor k
+k = int(snakemake.params['opt']['subsample'])
+if k > 1:
+    data = data[::k] # take every k-th element
+
+# Save processed data
+data.to_csv(snakemake.output['data'], index=False)
+graph.to_csv(snakemake.output['graph'], index=False)
